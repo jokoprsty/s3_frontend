4834f8b9c9e4c3d87ce6b4aa03448bdbe5fb1924
Joko Prasetyo <joko.prsty@gmail.com>
2025-06-04 20:38:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState("");
+
+    // Fetch items
+    useEffect(() => {
+        axios.get("http://localhost:5000/items")
+            .then(res => setItems(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    // Add item
+    const addItem = () => {
+        axios.post("http://localhost:5000/items", { name: newItem })
+            .then(res => setItems([...items, res.data]))
+            .then(() => setNewItem(""));
+    };
+
+    // Delete item
+    const deleteItem = (id) => {
+        axios.delete(`http://localhost:5000/items/${id}`)
+            .then(() => setItems(items.filter(item => item.id !== id)));
+    };
+
+    return (
+        <div>
+            <h1>Simple CRUD App</h1>
+            <input
+                type="text"
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+                placeholder="Enter item"
+            />
+            <button onClick={addItem}>Add Item</button>
+
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        {item.name} <button onClick={() => deleteItem(item.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
